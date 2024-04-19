b4d06e8ffdf33546e0df7e75c5123900948085e1
TobyMeehan <toby@tobymeehan.com>
2024-04-19 21:38:19 +0100

    Add env var configuration

--- samples/ArpSample.Monitor/Program.cs
+++ samples/ArpSample.Monitor/Program.cs
@@ -2,11 +2,13 @@ using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
+
 builder.Services.AddMassTransit(cfg =>
 {
     cfg.UsingGrpc((context, options) =>
     {
-        options.Host(new Uri("http://localhost:19796"));
+        options.Host(new Uri(builder.Configuration["Grpc:Host"]));
         
         options.ConfigureEndpoints(context);
     });
--- samples/ArpSample.Sensor/Program.cs
+++ samples/ArpSample.Sensor/Program.cs
@@ -2,13 +2,15 @@ using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
+
 builder.Services.AddMassTransit(cfg =>
 {
     cfg.UsingGrpc((context, options) =>
     {
-        options.Host(new Uri("http://localhost:19797"), h =>
+        options.Host(new Uri(builder.Configuration["Grpc:Host"]), h =>
         {
-            h.AddServer(new Uri("http://localhost:19796"));   
+            h.AddServer(new Uri(builder.Configuration["Grpc:Monitor"]));   
         });
         
         options.ConfigureEndpoints(context);
